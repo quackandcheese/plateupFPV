f9531b1e83297b2b1813ff5c8590a6cecfc26fd5
quackandcheese <8crackedmc8@gmail.com>
2023-04-15 10:34:29 -0500

    Lots

---
+++ plateupFPV/IndicatorLookAtPlayerView.cs
@@ -0,0 +1,92 @@
+﻿using FirstPersonView;
+using Kitchen;
+using KitchenMods;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Entities.UniversalDelegates;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace KitchenFirstPersonView
+{
+    public class IndicatorLookAtPlayerView : UpdatableObjectView<IndicatorLookAtPlayerView.MyViewData>
+    {
+        public class MyViewSystemBase : IncrementalViewSystemBase<MyViewData>, IModSystem
+        {
+            private EntityQuery _myEntityQuery;
+            private EntityQuery _playerQuery;
+
+            protected override void Initialise()
+            {
+                base.Initialise();
+                _myEntityQuery = GetEntityQuery(new QueryHelper().All(typeof(CFirstPersonIndicator), typeof(CLinkedView)));
+
+                _playerQuery = GetEntityQuery(new QueryHelper().All(typeof(CPlayer), typeof(CPosition)));
+            }
+
+            protected override void OnUpdate()
+            {
+                if (_myEntityQuery.IsEmpty) return;
+
+                using NativeArray<CLinkedView> nativeArray = _myEntityQuery.ToComponentDataArray<CLinkedView>(Allocator.Temp);
+                using var components = _playerQuery.ToComponentDataArray<CPosition>(Allocator.Temp);
+
+
+                for (int i = 0; i < nativeArray.Length; i++)
+                {
+                    SendUpdate(nativeArray[i], new MyViewData { PlayerPosition = components[0].Position });
+                }
+            }
+        }
+
+        [MessagePackObject]
+        public struct MyViewData : ISpecificViewData, IViewData, IViewResponseData, IViewData.ICheckForChanges<MyViewData>
+        {
+            [Key(0)] public Vector3 PlayerPosition;
+            //[Key(1)] public bool IsFirstPerson;
+
+            public IUpdatableObject GetRelevantSubview(IObjectView view)
+            {
+                return view.GameObject.AddComponent<IndicatorLookAtPlayerView>();
+                //if this view component is already on one of your prefabs from your asset bundle, you can just use the built in method
+                // return view.GetSubView<MyView>();
+            }
+
+            public bool IsChangedFrom(MyViewData check)
+            {
+                return PlayerPosition.x != check.PlayerPosition.x || PlayerPosition.y != check.PlayerPosition.y || PlayerPosition.z != check.PlayerPosition.z;
+            }
+        }
+
+        protected override void UpdateData(MyViewData data)
+        {
+            //this method lets you manipulate the gameobject 
+            
+            foreach (Transform child in transform)
+            {
+                //Quaternion originalRotation = child.rotation;
+
+
+                //if (data.IsFirstPerson)
+                //{
+                //    originalRotation = child.rotation;
+                    child.LookAt(data.PlayerPosition);
+                child.Rotate(Vector3.right, -90);
+
+                   
+                //}
+                //else
+                //{
+                //    child.rotation = originalRotation;
+                //}
+            }
+        }
+    }
+}
--- plateupFPV/ProgressIndicatorsLookAtPlayer.cs
+++ plateupFPV/ProgressIndicatorsLookAtPlayer.cs
@@ -12,55 +12,36 @@ using UnityEngine;
 
 namespace KitchenFirstPersonView
 {
-    public class ProgressIndicatorsLookAtPlayer : GenericSystemBase, IModSystem
+    public struct CFirstPersonIndicator : IModComponent
     {
-        private EntityQuery EntitiesToActOn;
-        private EntityQuery PlayerStuff;
+    }
+
+    public class SetLookAtData : GenericSystemBase, IModSystem
+    {
+        private EntityQuery indicators;
 
         protected override void Initialise()
         {
             base.Initialise();
-            EntitiesToActOn = GetEntityQuery(typeof(CProgressIndicator), typeof(CPosition));
-
-            PlayerStuff = GetEntityQuery(typeof(CPlayer), typeof(CPosition));
+            indicators = GetEntityQuery(new QueryHelper()
+                    .All(
+                        typeof(CIndicator),
+                        typeof(CPosition))
+                    .None(
+                        typeof(CFirstPersonIndicator)
+                    ));
         }
 
         protected override void OnUpdate()
         {
-            using var ents = EntitiesToActOn.ToEntityArray(Allocator.Temp);
-
-            using var my_components2 = EntitiesToActOn.ToComponentDataArray<CPosition>(Allocator.Temp);
-
-            for (var i = 0; i < ents.Length; i++)
+            var indicators = this.indicators.ToEntityArray(Allocator.Temp);
+            for (int i = 0; i < indicators.Length; i++)
             {
-                var ent = ents[i];
-                var my_component = my_components2[i];
-
-                var playerPos = GetPlayerPosition();
-
-                if ((playerPos != null || playerPos != default) && ent != default)
+                Set(indicators[i], new CFirstPersonIndicator()
                 {
-                    my_component.Rotation = Quaternion.LookRotation(playerPos, Vector3.up);
-                    Set(ent, my_component);
-                }
+                });
             }
-        }
-        private Vector3 GetPlayerPosition()
-        {
-            using var playerEnts = PlayerStuff.ToEntityArray(Allocator.Temp);
-            using var my_components = PlayerStuff.ToComponentDataArray<CPosition>(Allocator.Temp);
-
-            for (var i = 0; i < playerEnts.Length; i++)
-            {
-                var ent = playerEnts[i];
-                var my_component = my_components[i];
-
-                if (ent != default)
-                {
-                    return my_component.Position;
-                }
-            }
-            return default;
+            indicators.Dispose();
         }
     }
 }
